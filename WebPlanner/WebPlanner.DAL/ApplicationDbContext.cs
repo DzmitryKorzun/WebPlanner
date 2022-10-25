7c4b019107d7b8923325f253f1e98edb00b06fd9
Dima_Korzun <it.dzmitrykorzun@gmail.com>
2022-10-25 12:32:12 +0300

    add first enities

--- WebPlanner/WebPlanner.DAL/ApplicationDbContext.cs
+++ WebPlanner/WebPlanner.DAL/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebPlanner.Domain.Entity;
 
 namespace WebPlanner.DAL
 {
@@ -14,7 +15,13 @@ namespace WebPlanner.DAL
             Database.EnsureCreated();
         }
 
+        #region DbSets
 
+        public DbSet<ItemGroup> itemGroups { get; set; }
+        public DbSet<ItemSubgrop> itemSubgrops { get; set; }
+
+
+        #endregion
 
     }
 }
---
+++ WebPlanner/WebPlanner.Domain/Entity/ItemSubgrop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPlanner.Domain.Entity
+{
+    public class ItemSubgrop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
