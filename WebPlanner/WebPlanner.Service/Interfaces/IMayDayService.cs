02be9a616c63c2b12f8aae89db0f203e0e21991a
DzmitryKorzun <it.dzmitrykorzun@gmail.com>
2022-12-06 00:35:28 +0300

    add services

---
+++ WebPlanner/WebPlanner.Service/Interfaces/IMayDayService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPlanner.Service.Interfaces
+{
+    public interface IMayDayService
+    {
+    }
+}
---
+++ WebPlanner/WebPlanner.Service/Services/ItemServices/ColorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebPlanner.Service.Interfaces.ItemInterfaces;
+
+namespace WebPlanner.Service.Services.ItemServices
+{
+    public class ColorService : IColorService
+    {
+
+    }
+}
